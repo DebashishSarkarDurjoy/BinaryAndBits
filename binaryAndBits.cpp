dfcd9baf3bf8dfaa7dabe3589d8505bfa681eee8
DeV <dsdjoy4@gmail.com>
2021-07-06 03:05:17 +1000

    BitsToInt.cpp updated for 2's complement

--- binaryAndBits.cpp
+++ binaryAndBits.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int bitsToInt(char bitList[]) {
   char bit = '0';
-  int result = 0;
 
   int digits = 0;
   char i = ' ';
@@ -16,10 +15,17 @@ int bitsToInt(char bitList[]) {
   }
   digits = counter - 1 ;
 
+  int result = 0;
+
   int placeValue = pow(2, digits - 1);
 
   for (int i = 0; i < digits; i++) {
     bit = bitList[i];
+    if (i == 0 && bitList[i] == '1') {
+      result = -1 * pow(2, digits - 1);
+      placeValue *= 0.5;
+      continue;
+    }
     result += (bit == '1') ? placeValue : 0;
     placeValue *= 0.5;
   }
